f62ed16565c36cdd62f68ec25e6f0200bc12e498
Ateeb09 <ateebahmad009@gmail.com>
2026-06-25 18:03:58 +0530

    Run collectstatic and migrate programmatically in wsgi.py on boot

--- travel2india/travel2india/wsgi.py
+++ travel2india/travel2india/wsgi.py
@@ -8,9 +8,26 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travel2india.settings')
+
+django.setup()
+
+# Run collectstatic on startup
+try:
+    print("Running collectstatic on startup...")
+    call_command('collectstatic', '--noinput')
+except Exception as e:
+    print(f"Error running collectstatic: {e}")
+
+# Run database migrations on startup
+try:
+    print("Running migrate on startup...")
+    call_command('migrate', '--noinput')
+except Exception as e:
+    print(f"Error running migrate: {e}")
 
 application = get_wsgi_application()
