0b7e0889d2ee26d881cec1e5d7b44ae783b4c6d3
Ateeb09 <ateebahmad009@gmail.com>
2026-06-25 17:45:49 +0530

    Fix ALLOWED_HOSTS for custom domain and mark build.sh as executable

--- travel2india/travel2india/settings.py
+++ travel2india/travel2india/settings.py
@@ -33,12 +33,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-(xc)ay))dtav_+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True' and 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['192.168.1.6', 'localhost', '127.0.0.1']
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-else:
-    ALLOWED_HOSTS.append('*')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 
